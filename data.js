b67f3b72815169e5c62224ae78c12cc261eacbd3
RIG360 <134787168+RIG360@users.noreply.github.com>
2026-09-18 13:29:04 +0200

    commit

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-breit27-2-38_hausflur",
+      "name": "Breit27-2-38_Hausflur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08645716219647603,
+          "pitch": -0.024831574710052706,
+          "rotation": 0,
+          "target": "1-breit27-2-38_kuche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-breit27-2-38_kuche",
+      "name": "Breit27-2-38_Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.06113733538872,
+          "pitch": 0.03705501771933584,
+          "rotation": 0,
+          "target": "0-breit27-2-38_hausflur"
+        },
+        {
+          "yaw": -0.08496830579046488,
+          "pitch": 0.01071074472935507,
+          "rotation": 0,
+          "target": "2-breit27-2-38_zimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-breit27-2-38_zimmer",
+      "name": "Breit27-2-38_Zimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.897831414122088,
+          "pitch": 0.027381936832743037,
+          "rotation": 0,
+          "target": "1-breit27-2-38_kuche"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Breit27-2-38_260916",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
